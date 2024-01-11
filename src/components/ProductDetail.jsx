101e4a175cc92ecb35438104f6cf4a351e2bc052
Nathan George <georgenathan010@gmail.com>
2024-01-11 17:49:02 +0300

    finalized the routing and everything works correctly || last commit

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Products } from './Products';
+import { useParams } from 'react-router-dom';
+import CardDetails from './CardDetails';
 
 const ProductDetail = () => {
+    let {id} = useParams()
+    console.log(id);
+    const productWithId = Products.find((product)=> {
+        return product.id === parseInt(id);
+    })
+    console.log(productWithId)
   return (
-    <div>ProductDetail</div>
+    <CardDetails product={productWithId}/>
   )
 }
 
